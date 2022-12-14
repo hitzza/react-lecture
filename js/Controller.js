fcbf7c94f3ef908bb146ae9873a5a9460c1073c3
hitzza <cslzh4433@gmail.com>
2022-12-14 15:01:57 +0900

    submit event -show keword

--- js/Controller.js
+++ js/Controller.js
@@ -1,10 +1,23 @@
 const tag = "[Controller]";
+
 //생성 시점의 store와 View를 받음
+
 export default class Controller {
   constructor(store, {searchFormView}) {
     this.store = store;
     console.log(tag,'controller');
 
     this.searchFormView = searchFormView;
+
+    this.subscribeViewEvents();
+    //뷰 이벤트들을 수신할 함수
+  }
+  subscribeViewEvents(){
+    this.searchFormView.on('@submit',(event) => this.search(event.detail.value));
+    //검색 함수를 searchFormView에 할당
+  }
+  search(keyword){//검색 기능을 수행할 함수
+    console.log(tag,'keyword',keyword);
+    //추후 검색기능 추가 예정
   }
 }
--- js/views/SearchFormView.js
+++ js/views/SearchFormView.js
@@ -21,11 +21,23 @@ export default class SearchFormView extends View{
     showResetButton(visible = true){//값이 true면 리셋버튼을 보여주고 false면 삭제 
         this.resetElement.style.display = visible ? 'block' : 'none';
     }
-    bindEvent(){//대상 엘리먼트에 keyup이벤트 발생 시 핸들러 호출
+    bindEvent(){//대상 엘리먼트에 이벤트 발생 시 핸들러 호출
         on(this.inputElement,'keyup',() => this.handleKeyUp());
+        //inputElement의 keyup이벤트 발생시 호출
+        on(this.element,'submit', (event) => this.handleSubmit(event));
+        //#search-form-view 의 submit이벤트 발생시 호출
     }
     handleKeyUp(){//bindEvent함수에서 keyup이벤트에서 사용할 콜백함수
         const {value} = this.inputElement;
         this.showResetButton(value.length > 0);
+        //검색어 입력란에 값이 있으면 ture 아니면 false를 showResetButton함수에 반환 
+    }
+    handleSubmit(event){
+        event.preventDefault();//form의 기본성질중 sibmit발생 시 페이지 리셋 기능을 제거하기 위해 사용
+        console.log(tag,'handleSubmit');
+        const {value} = this.inputElement;
+        //검색어로 사용하기 위한 현재 inputElement에 작성한 객체 생성
+        this.emit('@submit', {value});
+        //검색어로 검색기능 사용할 @submit함수 생성
     }
 }
