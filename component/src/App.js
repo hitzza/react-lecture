2bc80dab9301717fa4327300367a5dd7ae40189a
hitzza <cslzh4433@gmail.com>
2022-12-30 18:03:49 +0900

    searchform refactoring

--- component/src/App.js
+++ component/src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import Header from './components/Header.js';
 import SearchForm from './components/SearchForm.js';
 
+
 export default class App extends React.Component{
+    constructor(){
+        super();
+
+        this.state = {
+            searchKeyword : '',
+        }
+    }
+    /** 검색 */
     search(searchKeyword){
+        searchKeyword = this.state.searchKeyword;
         console.log('TODO:search',searchKeyword);
     }
+    /** 리셋 */
     handleReset(){
         console.log('TODO : handleReset');
+    }
+    /** 키워드 변화 감지 */
+    handleChangeInput(searchKeyword){
+        if(searchKeyword.length <= 0){
+            this.handleReset();
+        }
+        this.setState({ searchKeyword });
     }
 
     render(){
@@ -16,6 +34,8 @@ export default class App extends React.Component{
                 <Header title='검색' />
                 <div className='container'>
                     <SearchForm 
+                    value = {this.state.searchKeyword}
+                    onChange = {(value)=> this.handleChangeInput(value)}
                     onSubmit={(searchKeyword)=> this.search(searchKeyword)} 
                     onReset={()=> this.handleReset()}
                     />
--- component/src/components/SearchForm.js
+++ component/src/components/SearchForm.js
@@ -1,51 +1,40 @@
 import React from "react";
 
-export default class SearchForm extends React.Component{
-    constructor(){
-        super();
-
-        this.state = {
-            searchKeyword : '',
-        }
-    }
+const SearchForm = ({value, onChange, onSubmit, onReset}) => {
     /** 검색 폼에서 엔터를 눌렀을 때 호출하는 함수 */
-    handleSubmit(event){
+    const handleSubmit = (event) => {
         event.preventDefault();
         /** 자식요소에서 부모 요소로 props를 전송하기 위한 콜백함수 */
-        this.props.onSubmit(this.state.searchKeyword);
-    }
-    /** 리셋 버튼 클릭시 호출하는 함수 */
-    handleReset(){
-        this.props.onReset();
-    }
-    /** 검색창에 변화가 생기면 호출하는 함수 */
-    handleChangeInput(event){
-        const searchKeyword = event.target.value;
-        /** 검색어가 없다면 리셋 버튼 함수 호출 */
-        if(searchKeyword.length <= 0){
-            this.handleReset();
-        }
-        this.setState({ searchKeyword });
+        onSubmit();
     }
     
-
-    render(){
-        return (
-            <form
-                onSubmit={(event)=> {this.handleSubmit(event)}}
-                onReset={()=> {this.handleReset()}}
-            >
-                <input 
-                    type="text" 
-                    placeholder="검색어를 입력하세요" 
-                    autoFocus 
-                    value = {this.state.searchKeyword}
-                    onChange = {(event)=> this.handleChangeInput(event)}//인풋 박스 내부에 변화가 생기면 호출
-                />
-                {this.state.searchKeyword.length > 0 &&(
-                    <button type="reset" className="btn-reset"></button>
-                )}
-               
-            </form>);
+    /** 리셋 버튼 클릭시 호출하는 함수 */
+    const handleReset = () => {
+        onReset();
     }
+    /** 검색창에 변화가 생기면 호출하는 함수 */
+    const handleChangeInput = (event) => {
+        onChange(event.target.value);
+    }
+    
+    
+    
+    return (
+        <form
+            onSubmit={handleSubmit}
+            onReset={handleReset}
+        >
+            <input 
+                type="text" 
+                placeholder="검색어를 입력하세요" 
+                autoFocus 
+                value = {value}
+                onChange = {handleChangeInput}//인풋 박스 내부에 변화가 생기면 호출
+            />
+            {value.length > 0 &&(
+                <button type="reset" className="btn-reset"></button>
+            )}
+           
+        </form>);
 }
+export default SearchForm;
