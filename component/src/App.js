d3f2d2b0f87726f93afc8faba8e9052fe772de1b
hitzza <cslzh4433@gmail.com>
2022-12-30 15:48:58 +0900

    header component

--- component/src/App.js
+++ component/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from './components/Header.js';
 
 export default class App extends React.Component{
     render(){
-        return <>TODO App Component</>
+        return <Header />;
     }
 
 }
